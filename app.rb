9f7d3da1be1d67b48788d773a30ce5b584cbf652
lawson381 <lawson381@hotmail.com>
2016-10-06 14:25:28 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,25 +1,61 @@
 # app.rb
 
+
 require 'sinatra'
+
 require 'sinatra/activerecord'
+
 require './environments'
 
-class Contact < ActiveRecord::Base
-  self.table_name = 'salesforce.contact'
+
+class Account < ActiveRecord::Base
+
+self.table_name = 'salesforce.account'
+
 end
 
-get "/" do
-  erb :home
+
+
+class Contact < ActiveRecord::Base
+
+self.table_name = 'salesforce.contact'
+
 end
+
 
 get "/contacts" do
-  @contacts = Contact.all
-  erb :index
+
+@contacts = Contact.all
+
+erb :index
+
 end
 
-get "/create" do
-  dashboard_url = 'https://dashboard.heroku.com/'
-  match = /(.*?)\.herokuapp\.com/.match(request.host)
-  dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
-  redirect to(dashboard_url)
+
+get "/accounts" do
+
+@accounts = Account.all
+
+erb :indexa
+
 end
+
+
+get "/" do
+
+erb :home
+
+end
+
+
+get "/create" do
+
+dashboard_url = 'https://dashboard.heroku.com/'
+
+match = /(.*?)\.herokuapp\.com/.match(request.host)
+
+dashboard_url << "apps/#{match[1]}/resources" if match && match[1]
+
+redirect to(dashboard_url)
+
+End 
